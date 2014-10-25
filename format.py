2e737e0a8feaf3f41b4245457eb08e7ded9a5436
Mike Krinkin <krinkin.m.u@gmail.com>
2014-10-25 18:39:58 +0400

    fix format

--- format.py
+++ format.py
@@ -6,5 +6,6 @@ if __name__ == "__main__":
     import sys
     for filename in sys.argv[1:]:
         program = Parser().parse(Scanner(filename), False)
+        printer = PrettyPrinter()
         for stmt in program:
-            PrettyPrinter().visit(stmt)
+            printer.visit(stmt)
